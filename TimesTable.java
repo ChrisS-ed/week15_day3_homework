aac4b2ae46dde68aebd84ca65c03c2391b3aa04f
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-03-16 20:19:47 +0000

    done

--- TimesTable.java
+++ TimesTable.java
@@ -3,29 +3,18 @@ public class TimesTable {
   public static void main(String[] args)
   {
     int[][] table = new int[9][9];
-    String[] multiples = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    System.out.printf("%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%n"," ","1","2","3","4","5","6","7","8","9");
 
-    System.out.printf("%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%n"," ","1","2","3","4","5","6","7","8","9");
-
-    for (int y = 0 ; y < 9 ; y++)
+    for (int x = 0 ; x < 9 ; x++)
     {
-      for (int x = 0 ; x < 9 ; x++)
+      System.out.printf("%-5s", String.valueOf(x+1));
+      for (int y = 0 ; y < 9 ; y++)
       {
         table[x][y] = (x+1) * (y+1);
         String numberString = String.valueOf(table[x][y]);
-        //System.out.println(table[x][y]);
+        System.out.printf("%-5s", (x+1) * (y+1));
       }
-    }
-
-    for (int y = 0 ; y < 9 ; y++)
-    {
-      
-        
-        //String numberString = String.valueOf(table[x][y]);
-        //System.out.println(table[x][y]);
-      //System.out.println(table[x][y]);
-      System.out.printf("%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%-7s%n", y+1, multiples[0], multiples[1], multiples[2], multiples[3], multiples[4], multiples[5], multiples[6], multiples[7], multiples[8]);
-
+      System.out.println();
     }
 
   }
